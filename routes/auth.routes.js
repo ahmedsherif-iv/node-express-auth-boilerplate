4a469b94624c45503f2a2c7f317ee1b8123fd6c8
Ahmed Sherif <ahmed.sherif146@outlook.com>
2021-01-17 22:35:35 +0200

    added helmet, cors, compression packages, and modified routes/index.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,22 +9,26 @@ const router = Router();
 // local auth
 router.route('/register')
     .post(celebrate(userValidation.registerSchema, opts), authController.registerUser);
+
 router.route('/login')
     .post(celebrate(userValidation.loginSchema, opts), authController.loginWithEmailAndPassword);
 
 // password reset
 router.route('/password-reset/get-code')
     .post(celebrate(userValidation.sendRequestEmailSchema, opts), authController.sendResetPasswordEmail);
+
 router.route('/password-reset/verify/:token')
     .post(celebrate(userValidation.resetPasswordSchema, opts), authController.resetPassword);
 
 // google auth
 router.route('/google').get(authController.loginWithGoogle);
+
 router.route('/google/callback')
     .get(passport.authenticate('google'), authController.authThirdPartyCallback);
 
 // facebook auth
 router.route('/facebook').get(authController.loginWithFacebook);
+
 router.route('/facebook/callback')
     .get(passport.authenticate('facebook'), authController.authThirdPartyCallback);
 
